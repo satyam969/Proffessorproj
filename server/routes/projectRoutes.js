4a5f4e2812f9c0f4e4c0ca2f877c44de7f2af395
Priya Raj <144319984+satyam969@users.noreply.github.com>
2025-02-09 21:42:27 +0530

    Update projectRoutes.js
    spelling mistake

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createProject, getProjects, getProjectById, updateProject, deleteProject } = require("../controllers/projectController");
+const { createProject, getProjects, getProjectById, updateProject, deleteProject } = require("../controllers/projectcontroller");
 const { protect, admin } = require("../middleware/AuthMiddleware");
 
 router.route("/").post( protect,admin,createProject).get(getProjects);
